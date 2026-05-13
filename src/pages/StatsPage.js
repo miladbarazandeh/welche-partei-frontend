c5759157fd8898b6841b85889d54b1be90cc6ff6
Milad Barazandeh <milad.barazandeh@zalando.de>
2026-05-13 11:22:30 +0200

    Add ResponsiveAdBanner component and integrate it into App and StatsPage for enhanced advertising support

--- src/pages/StatsPage.js
+++ src/pages/StatsPage.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import AppBrand from '../components/AppBrand';
 import AppPromoCards from '../components/AppPromoCards';
 import LegalFooter from '../components/LegalFooter';
+import ResponsiveAdBanner from '../components/ResponsiveAdBanner';
 import SettingsMenu from '../components/SettingsMenu';
 import { useCountry, useI18n, usePartyHelpers, usePlayerName } from '../context/AppContext';
 import { countryApiUrl } from '../lib/api';
@@ -336,6 +337,8 @@ export default function StatsPage() {
             <InsightCard value={data.global_best_streak} label={t('stats.bestStreak')} />
           </div>
 
+          <ResponsiveAdBanner />
+
           {data.spectrum_accuracy != null && (
             <section className="leaning-section">
               <p className="stats-section-heading">{t('stats.leftRight')}</p>
@@ -480,6 +483,8 @@ export default function StatsPage() {
               </div>
             </section>
           )}
+
+          <ResponsiveAdBanner />
         </>
       )}
 
